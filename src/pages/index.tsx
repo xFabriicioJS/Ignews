3a467d7ca3f4082c924dc4904d21fa42c4300fda
xFabriicioJS <fabriciocostamonteiro@hotmail.com>
2022-06-13 19:17:58 -0300

    primeiro commit - sass

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from '../styles/home.module.scss'
+
+
+function Home() {
+  return (
+    <h1 className={styles.title}>App está rodando</h1>
+  )
+}
+
+export default Home
